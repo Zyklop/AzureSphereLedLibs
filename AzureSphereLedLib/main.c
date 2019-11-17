91c1ff5a09ea8ab563200422bcd8e109d7dc2f67
Renato Bosshart <1127789+Zyklop@users.noreply.github.com>
2019-11-17 21:00:52 +0100

    Activated onboard led

--- AzureSphereLedLib/main.c
+++ AzureSphereLedLib/main.c
@@ -7,7 +7,7 @@
 
 int main(void)
 {
-	//PwmRainbow(2, 0, 1, 2, 10);
+	PwmRainbow(2, 0, 1, 2, 10);
 	//ApaSolidRainbow(1, -1, 150, 10);
 	//ApaMovingLight(1, -1, 150, 500);
 	//ApaMovingRainbow(1, -1, 150, 10);
