7720a30d7c91b1712903b39156fd8f7fd260af75
Renato Bosshart <1127789+Zyklop@users.noreply.github.com>
2019-11-17 21:00:08 +0100

    Updated main to not start only the onboard led

--- AzureSphereLedLib/main.c
+++ AzureSphereLedLib/main.c
@@ -1,4 +1,4 @@
-﻿#include <stdbool.h>
+#include <stdbool.h>
 #include <errno.h>
 #include <string.h>
 #include <time.h>
@@ -8,7 +8,7 @@
 int main(void)
 {
 	//PwmRainbow(2, 0, 1, 2, 10);
-	//ApaSolidRainbow(1, -1, 2, 10);
-	//ApaMovingLight(1, -1, 3, 500);
-	ApaMovingRainbow(1, -1, 2, 10);
+	//ApaSolidRainbow(1, -1, 150, 10);
+	//ApaMovingLight(1, -1, 150, 500);
+	//ApaMovingRainbow(1, -1, 150, 10);
 }
